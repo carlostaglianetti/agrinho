92479acecafbfc8d4febe690c45c8b5efb076a3b
Carlos Domingos Taglianetti <115033385+carlostaglianetti@users.noreply.github.com>
2026-06-11 08:50:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+<lottie-player 
+    src="https://assets5.lottiefiles.com/packages/lf20_t9m978g6.json"  background="transparent"  
+    speed="1"  
+    style="width: 300px; height: 300px;"  
+    loop  
+    autoplay>
+</lottie-player>
